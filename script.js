d564797090a8b8a0195b79403b013b353d2d35f7
Laura Caroselli <lcaroselli@outlook.com>
2017-10-18 13:03:44 -0600

    Mobile tabs JS

--- script.js
+++ script.js
@@ -24,3 +24,45 @@ $('.tab-4').on('click', () => {
   resetTabs();
   $('.tab-4').addClass('selected-tab');
 });
+
+
+//Mobile
+$('.tab-1-plus').on('click', () => {
+  $('.mobile-1').removeClass('hidden-info')
+  $('.mobile-1').addClass('show-info');
+});
+
+$('.tab-1-minus').on('click', () => {
+  $('.mobile-1').removeClass('show-info')
+  $('.mobile-1').addClass('hidden-info');
+});
+
+$('.tab-2-plus').on('click', () => {
+  $('.mobile-2').removeClass('hidden-info')
+  $('.mobile-2').addClass('show-info');
+});
+
+$('.tab-2-minus').on('click', () => {
+  $('.mobile-2').removeClass('show-info')
+  $('.mobile-2').addClass('hidden-info');
+});
+
+$('.tab-3-plus').on('click', () => {
+  $('.mobile-3').removeClass('hidden-info')
+  $('.mobile-3').addClass('show-info');
+});
+
+$('.tab-3-minus').on('click', () => {
+  $('.mobile-3').removeClass('show-info')
+  $('.mobile-3').addClass('hidden-info');
+});
+
+$('.tab-4-plus').on('click', () => {
+  $('.mobile-4').removeClass('hidden-info')
+  $('.mobile-4').addClass('show-info');
+});
+
+$('.tab-4-minus').on('click', () => {
+  $('.mobile-4').removeClass('show-info')
+  $('.mobile-4').addClass('hidden-info');
+});
