517f4addb7c811d0ebac8a87658f88559f10a4ce
Laura Caroselli <lcaroselli@outlook.com>
2017-10-23 11:19:58 -0600

    Refactor script

--- script.js
+++ script.js
@@ -1,76 +1,18 @@
-const resetTabs = () => {
-  if($('.tab').hasClass('selected-tab')) {
-    $('.tab').addClass('unselected-tab');
-  };
+$('.tab').on('click', function() {
   $('.tab').removeClass('selected-tab');
-};
+  $('.tab').addClass('unselected-tab');
+  $(this).addClass('selected-tab');
+})
 
-$('.tab-1').on('click', () => {
-  resetTabs();
-  $('.tab-1').addClass('selected-tab');
+// //Mobile
+$('.plus').on('click', (e) => {
+  $(e.target).parent().siblings('.mobile-section').removeClass('hidden-info');
+  $(e.target).parent().siblings('.mobile-section').addClass('show-info');
+  $(e.target).siblings('.minus').addClass('show-info');
 });
 
-$('.tab-2').on('click', () => {
-  resetTabs();
-  $('.tab-2').addClass('selected-tab');
-});
-
-$('.tab-3').on('click', () => {
-  resetTabs();
-  $('.tab-3').addClass('selected-tab');
-});
-
-$('.tab-4').on('click', () => {
-  resetTabs();
-  $('.tab-4').addClass('selected-tab');
-});
-
-
-//Mobile
-$('.tab-1-plus').on('click', () => {
-  $('.mobile-1').removeClass('hidden-info');
-  $('.mobile-1').addClass('show-info');
-  $('.tab-1-minus').addClass('show-info');
-});
-
-$('.tab-1-minus').on('click', () => {
-  $('.mobile-1').removeClass('show-info');
-  $('.mobile-1').addClass('hidden-info');
-  $('.tab-1-minus').removeClass('show-info');
-});
-
-$('.tab-2-plus').on('click', () => {
-  $('.mobile-2').removeClass('hidden-info');
-  $('.mobile-2').addClass('show-info');
-  $('.tab-2-minus').addClass('show-info');
-});
-
-$('.tab-2-minus').on('click', () => {
-  $('.mobile-2').removeClass('show-info');
-  $('.mobile-2').addClass('hidden-info');
-  $('.tab-2-minus').removeClass('show-info');
-});
-
-$('.tab-3-plus').on('click', () => {
-  $('.mobile-3').removeClass('hidden-info');
-  $('.mobile-3').addClass('show-info');
-  $('.tab-3-minus').addClass('show-info');
-});
-
-$('.tab-3-minus').on('click', () => {
-  $('.mobile-3').removeClass('show-info');
-  $('.mobile-3').addClass('hidden-info');
-  $('.tab-3-minus').removeClass('show-info');
-});
-
-$('.tab-4-plus').on('click', () => {
-  $('.mobile-4').removeClass('hidden-info');
-  $('.mobile-4').addClass('show-info');
-  $('.tab-4-minus').addClass('show-info');
-});
-
-$('.tab-4-minus').on('click', () => {
-  $('.mobile-4').removeClass('show-info');
-  $('.mobile-4').addClass('hidden-info');
-  $('.tab-4-minus').removeClass('show-info');
+$('.minus').on('click', (e) => {
+  $(e.target).parent().siblings('.mobile-section').removeClass('show-info');
+  $(e.target).parent().siblings('.mobile-section').addClass('hidden-info');
+  $(e.target).removeClass('show-info');
 });
